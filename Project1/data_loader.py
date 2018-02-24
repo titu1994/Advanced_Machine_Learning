8c6a6cd4d66ecbff0253aa20f50564c6ae13c564
Somshubra Majumdar <titu1994@gmail.com>
2018-02-24 15:22:57 -0600

    COrrect directory

---
+++ Project1/data_loader.py
@@ -0,0 +1,11 @@
+from sklearn.datasets.svmlight_format import load_svmlight_file
+
+Q1_TRAIN_PATH = "data/train.txt"
+Q1_TEST_PATH = "data/test.txt"
+
+
+def load_Q1_data():
+    X_train, y_train = load_svmlight_file(Q1_TRAIN_PATH)
+    X_test, y_test = load_svmlight_file(Q1_TEST_PATH)
+
+    return X_train, y_train, X_test, y_test
