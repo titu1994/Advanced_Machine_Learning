f8211b6b8be426d8f75c8f24d217314bf7c61c5a
Somshubra Majumdar <titu1994@gmail.com>
2018-02-25 11:59:10 -0600

    Start brute force implementation

--- Project1/data_loader.py
+++ Project1/data_loader.py
@@ -15,23 +15,14 @@ def load_Q1_data():
     Wj = lines[100 * 128: 26 * 128 + 100 * 128]
     Tij = lines[26 * 128 + 100 * 128:]
 
-    Xi = np.array(Xi, dtype=np.float64).reshape((100, 128))
-    Wj = np.array(Wj, dtype=np.float64).reshape((26, 128))
-    Tij = np.array(Tij, dtype=np.float64).reshape((26, 26), order='F').transpose()
+    Xi = np.array(Xi, dtype=np.float32).reshape((100, 128))
+    Wj = np.array(Wj, dtype=np.float32).reshape((26, 128))
+    Tij = np.array(Tij, dtype=np.float32).reshape((26, 26), order='F')
 
     print("Xi", Xi.shape, "Wj", Wj.shape, "Tij", Tij.shape)
     return Xi, Wj, Tij
 
 
-def plot_Xi(Xi, index):
-    xi = Xi[index, :]
-    xi = xi.reshape((16, 8))
-
-    plt.imshow(xi, cmap=plt.cm.binary)
-    plt.show()
-
-
 if __name__ == '__main__':
     Xi, Wj, Tij = load_Q1_data()
 
-    plot_Xi(Xi, 3)
---
+++ Project1/q1.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+from Project1.data_loader import load_Q1_data
+
+X, W, Tij = load_Q1_data()
+
+num_labels = 26
+m = X.shape[0]  # 100
+dim = X.shape[1]  # 128
+
+def brute_force():
+    y_hat = np.zeros((m, num_labels), dtype=np.float32)
+
+    # compute <W_yj, Xj>
+    wx_sum = np.zeros((26,), dtype=np.float32)
+    for j in range(m):
+        for y_j in range(num_labels):
+            wx_sum[y_j] += np.dot(X[j], W[y_j].T)
+
+
+    t_sum = np.zeros((26,), dtype=np.float32)
+    for j in range(m):
+        for y_j in range(num_labels - 1):
+                t_sum[y_j] += Tij[y_j, y_j + 1]
+
+    y_hat += wx_sum + t_sum
+
+    predictions = np.argmax(y_hat, axis=-1)
+    confidence = np.max(y_hat, axis=-1)
+
+    return predictions, confidence
+
+
+if __name__ == '__main__':
+    preds, confidence = brute_force()
+
+    for i, (p, c) in enumerate(zip(preds, confidence)):
+        print("i=%d : Predicted = %s (confidence = %0.2f)" % (i + 1, chr(p + ord('a')), c))
