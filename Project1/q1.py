Fix gradient Wj

Project1/q1.py
@@ -8,48 +8,29 @@ num_labels = 26
 m = X.shape[0]  # 100
 dim = X.shape[1]  # 128
 
-def brute_force():
-    # compute <W_yj, Xj>
-    wx_sum = np.zeros((m, 26,), dtype=np.float32)
-    for j in range(m):
-        for y_j in range(num_labels):
-            wx_sum[j, y_j] += np.dot(X[j], W[y_j].T)
-
-    # compute T[y_j, y_j + 1]
-    t_sum = np.zeros((1, 26), dtype=np.float32)
-    for y_j in range(num_labels):
-        if y_j + 1 != num_labels:
-            t_sum[0, y_j] += Tij[y_j, y_j + 1]
-
-    t_sum = np.repeat(t_sum, m, axis=0)
-    t_sum[-1, :] = 0.0
-
-    y_hat = wx_sum + t_sum
-
-    predictions = np.argmax(y_hat, axis=-1)
-    objective_value = np.max(y_hat, axis=-1)
-
-    return predictions, objective_value
-
-
 def brute_force_vectorized():
-    # compute <W_yj, Xj>
-    wx_sum = np.dot(X, W.T)
+    opt = np.zeros([X.shape[0], W.shape[0]], dtype=np.int32)
+    nodeweights = np.zeros([X.shape[0], W.shape[0]])
+    values = np.zeros([X.shape[0], W.shape[0]])
+    for j in range(W.shape[0]):
+        nodeweights[0, j] = np.dot(X[0], W[j])
+    values[0] = nodeweights[0].copy()
+    for i in range(1, X.shape[0]):
+        nodeweights[i] = np.dot(X[i], W.T)
+        for j in range(W.shape[0]):
+            for k in range(W.shape[0]):
+                val = values[i - 1, k] + nodeweights[i, j] + Tij[k, j]
+                if values[i, j] < val:
+                    values[i, j] = val
+                    opt[i, j] = k
+    k = np.argmax(nodeweights[X.shape[0] - 1])
+    predictions = np.zeros([X.shape[0]], dtype=np.int32)
+    predictions[X.shape[0] - 1] = k
+    for i in range(X.shape[0] - 1, 0, -1):
+        k = opt[i, k]
+        predictions[i - 1] = k
 
-    # compute T[y_j, y_j + 1]
-    t_sum = np.zeros((1, 26), dtype=np.float32)
-    for y_j in range(num_labels):
-        t_sum[0] += Tij[y_j]
-
-    t_sum = np.repeat(t_sum, m, axis=0)
-    t_sum[-1, :] = 0.0
-
-    y_hat = wx_sum + t_sum
-
-    predictions = np.argmax(y_hat, axis=-1)
-    objective_value = np.max(y_hat, axis=-1)
-
-    return predictions, objective_value
+    return predictions
 
 
 def max_sum_decoder():

Project1/q2.py
@@ -193,7 +193,7 @@ def conditional_prob(X_train, y_train, Wj, Tij):
     print("avg: " + str(avg))
     print(dist.shape)
 
-    result = open(r'dist2.txt', 'w+')
+    result = open(r'dist.txt', 'w+')
     for j in range(X_train.shape[0]):
          result.write('dist[' + str(j) + ']: ' + str([dist[j, s] for s in range(Wj.shape[0])]) + '\n')
     result.close()
@@ -210,10 +210,10 @@ if __name__ == '__main__':
     train_data, test_data = load_Q2_data()
     t_list = list(train_data.items())
 
-    y_train = np.empty([len(t_list), 26], dtype=np.int8) # [n, 26], change it to n
-    X_train = np.empty([len(t_list), 129], dtype=np.int16) # [n, 128], change it to n
+    y_train = np.zeros([len(t_list), 26], dtype=np.int8) # [n, 26], change it to n
+    X_train = np.zeros([len(t_list), 129], dtype=np.int16) # [n, 128], change it to n
     for index, i in enumerate(t_list):
-        y_train[index] = ord(i[1][0][0]) - 97
+        y_train[index, ord(i[1][0][0]) - 97] = 1
         X_train[index][0] = i[1][2]
         X_train[index][1:] = i[1][4]
 