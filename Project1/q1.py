8ba571d47674d00572d9d94fc8a408ac9eeb5bf0
Ganesh JCS <ganesh.jcs@gmail.com>
2018-03-04 22:55:18 -0600

    Finished 1c, with brute force

--- Project1/q1.py
+++ Project1/q1.py
@@ -8,31 +8,6 @@ num_labels = 26
 m = X.shape[0]  # 100
 dim = X.shape[1]  # 128
 
-def brute_force_vectorized():
-    opt = np.zeros([X.shape[0], W.shape[0]], dtype=np.int32)
-    nodeweights = np.zeros([X.shape[0], W.shape[0]])
-    values = np.zeros([X.shape[0], W.shape[0]])
-    for j in range(W.shape[0]):
-        nodeweights[0, j] = np.dot(X[0], W[j])
-    values[0] = nodeweights[0].copy()
-    for i in range(1, X.shape[0]):
-        nodeweights[i] = np.dot(X[i], W.T)
-        for j in range(W.shape[0]):
-            for k in range(W.shape[0]):
-                val = values[i - 1, k] + nodeweights[i, j] + Tij[k, j]
-                if values[i, j] < val:
-                    values[i, j] = val
-                    opt[i, j] = k
-    k = np.argmax(nodeweights[X.shape[0] - 1])
-    predictions = np.zeros([X.shape[0]], dtype=np.int32)
-    predictions[X.shape[0] - 1] = k
-    for i in range(X.shape[0] - 1, 0, -1):
-        k = opt[i, k]
-        predictions[i - 1] = k
-
-    return predictions
-
-
 def max_sum_decoder():
     opt = np.zeros([X.shape[0], W.shape[0]], dtype=np.int32)
     nodeweights = np.zeros([X.shape[0], W.shape[0]])
@@ -54,20 +29,46 @@ def max_sum_decoder():
     for i in range(X.shape[0] - 1, 0, -1):
         k = opt[i, k]
         predictions[i - 1] = k
+    return predictions
 
+def brute_force(X,W,T):
+    predictions = np.zeros(X.shape[0], dtype=np.int32)
+    tempsolutions = np.zeros(X.shape[0], dtype=np.int32)
+    value = compute_brute_value(tempsolutions, X, W, T)
+    while True:
+        i=0
+        while True:
+            tempsolutions[i] += 1
+            if(tempsolutions[i] == W.shape[0]):
+                tempsolutions[i] = 0
+                i+=1
+            else:
+                break
+        newvalue = compute_brute_value(tempsolutions, X, W, T)
+        if newvalue > value:
+            value = newvalue
+            predictions = tempsolutions.copy()
+        if np.all(tempsolutions == int(W.shape[0]-1)):
+            break
     return predictions
 
 
-if __name__ == '__main__':
-    preds, objective = brute_force_vectorized()
+def compute_brute_value(temp, X, W, T):
+    total = np.dot(X[0], W[temp[0]])
+    for i in range(1, X.shape[0]):
+        total += np.dot(X[i],W[temp[i]]) + T[temp[i-1]][temp[i]]
+    return total
 
-    # for i, (p, o) in enumerate(zip(preds, objective)):
-    #     print("i=%d : Predicted = %s (objective = %0.2f)" % (i + 1, chr(p + ord('a')), o))
+
+if __name__ == '__main__':
 
     preds_max_sum = max_sum_decoder()
 
     for i,p in enumerate(preds_max_sum):
         print("i=%d : Predicted = %s" % (i+1, chr(p+ord('a'))))
+
+    for i,p in enumerate(brute_force(X[:2], W, Tij)):
+        print("i=%d : Brute force predicted = %s" % (i+1, chr(p+ord('a'))))
 
     # print("Maximum objective score : ", objective.max())
     # print(max_sum_decoder())
