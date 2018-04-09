a332fecb3a121c5f2dc4d2e6e98ab83814453122
abhoi <abhoi3@uic.edu>
2018-04-09 00:02:32 -0500

    Added plots for word-wise error

--- proj2/results/plot_results.py
+++ proj2/results/plot_results.py
@@ -2,32 +2,37 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-a = open('ADAM_1e-06_f_evals.txt').readlines()
-b = open('SGD_1e-06_f_evals.txt').readlines()
-c = open('LAMBDA_1e-06_f_evals.txt').readlines()
-a = [x.split() for x in a]
-b = [x.split() for x in b]
+a = open('ADAM_0.0001_word_error.txt').readlines()
+b = open('SGD_0.0001_word_error.txt').readlines()
+c = open('LBFGS_0.0001_word_error.txt').readlines()
+#a = [x.split() for x in a]
+#b = [x.split() for x in b]
 c = [x.split() for x in c]
-c = c[46:297]
+c = c[46:214]
 a = pd.DataFrame(a)
 b = pd.DataFrame(b)
 c = pd.DataFrame(c)
-a.columns = ['passes', 'fn_val']
-b.columns = ['passes', 'fn_val']
+a.columns = ['test_word_err']
+a['index'] = range(1, len(a)+1)
+b.columns = ['test_word_err']
+b['index'] = range(1, len(b)+1)
 c.columns = ['iter', 'fn.val', 'gap', 'time', 'feval.num', 'train_lett_err', 'train_word_err', 'test_lett_err', 'test_word_err']
 
-passes = list(pd.to_numeric(a['passes']))
-fn_val = list(pd.to_numeric(a['fn_val']))
-passes2 = list(pd.to_numeric(b['passes']))
-fn_val2 = list(pd.to_numeric(b['fn_val']))
+passes = list(pd.to_numeric(a['index']))
+fn_val = list(pd.to_numeric(a['test_word_err']))
+fn_val = [x*100 for x in fn_val]
+passes2 = list(pd.to_numeric(b['index']))
+fn_val2 = list(pd.to_numeric(b['test_word_err']))
+fn_val2 = [x*100 for x in fn_val2]
 passes3 = list(pd.to_numeric(c['iter']))
-fn_val3 = list(pd.to_numeric(c['fn.val']))
+fn_val3 = list(pd.to_numeric(c['test_word_err']))
 
-#plt.plot(passes, fn_val, '-b', label='Adam')
-#plt.plot(passes2, fn_val2, 'r--', label='SGD')
+plt.plot(passes, fn_val, '-b', label='Adam')
+plt.plot(passes2, fn_val2, 'r--', label='SGD')
 plt.plot(passes3, fn_val3, 'k', label='LBFGS')
+
 plt.plot()
-plt.xlabel('Effective number of passes')
-plt.ylabel('Function value')
+plt.xlabel('Number of effective pass')
+plt.ylabel('Word-wise error')
 plt.legend(loc='upper right')
 plt.show()
